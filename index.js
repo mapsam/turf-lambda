cc18351478a38ee3897c016681f398c66129c3c9
mapsam <matthews.sam@gmail.com>
2017-04-14 16:57:06 -0700

    stringify ze object

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var turf = require('@turf/turf');
 var AWS = require('aws-sdk');
 
-exports.handler = function(event, context, callback) {
+module.exports.handler = function(event, context, callback) {
 
   var s3 = new AWS.S3({ accessKeyId: process.env.CUGOS_AWS_ACCESS_KEY_ID, secretAccessKey: process.env.CUGOS_AWS_SECRET_ACCESS_KEY, region: process.env.LAMBDA_REGION });
-
-  console.log('event: ', event);
-  console.log('context: ', context);
+  console.log('event: ', JSON.stringify(event));
 
   callback(null, 'complete!');
 }
