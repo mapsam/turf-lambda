cc15cc3d33a35907b20587dc52098f663ef90d69
mapsam <matthews.sam@gmail.com>
2017-04-14 16:40:00 -0700

    update env vars

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var AWS = require('aws-sdk');
 
 exports.handler = function(event, context, callback) {
 
-  var s3 = new AWS.S3({ accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, region: process.env.LAMBDA_REGION });
+  var s3 = new AWS.S3({ accessKeyId: process.env.CUGOS_AWS_ACCESS_KEY_ID, secretAccessKey: process.env.CUGOS_AWS_SECRET_ACCESS_KEY, region: process.env.LAMBDA_REGION });
 
   console.log('event: ', event);
   console.log('context: ', context);
