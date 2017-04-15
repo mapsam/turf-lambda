731462ebebdf8050df3010b229e09897fee53697
mapsam <matthews.sam@gmail.com>
2017-04-14 17:40:13 -0700

    run a turf task, provide example event object

--- index.js
+++ index.js
@@ -3,8 +3,29 @@ var AWS = require('aws-sdk');
 
 module.exports.handler = function(event, context, callback) {
 
-  var s3 = new AWS.S3({ accessKeyId: process.env.CUGOS_AWS_ACCESS_KEY_ID, secretAccessKey: process.env.CUGOS_AWS_SECRET_ACCESS_KEY, region: process.env.LAMBDA_REGION });
-  console.log('event: ', JSON.stringify(event));
+  var s3 = new AWS.S3({ accessKeyId: process.env.ACCESS_KEY, secretAccessKey: process.env.SECRET_KEY, region: process.env.LAMBDA_REGION });
 
-  callback(null, 'complete!');
-}
+  var info = event.Records[0].s3.bucket.arn.split(':::')[1].split('/');
+  var bucket = info[0];
+  var task = info[1];
+
+  var params = {
+    Key: task + '/' + event.Records[0].s3.object.key,
+    Bucket: event.Records[0].s3.bucket.arn;
+  };
+
+  s3.getObject(params, function(err, data) {
+    try {
+      var original = JSON.parse(data);
+    } catch (err) {
+      return callback('Error parsing original GeoJSON');
+    }
+
+    if (turf[task]) {
+      var final = turf[task](original);
+      return callback(null, final);
+    } else {
+      return callback(`Turf method (${task}) does not exist.`);
+    }
+  });
+};
