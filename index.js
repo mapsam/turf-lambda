f431e9cb6ce487bcecf4571b2060af205d89da7f
mapsam <matthews.sam@gmail.com>
2017-04-30 13:48:52 -0700

    invalid fixtures

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ exports.handler = function(event, context, callback) {
 
   var bucket = event.Records[0].s3.bucket.name;
   var key = event.Records[0].s3.object.key;
-  var task = key.split('/')[0];
+  // var task = key.split('/')[0];
 
   var params = {
     Key: key,
@@ -23,18 +23,79 @@ exports.handler = function(event, context, callback) {
 
   s3.getObject(params, function(err, data) {
     if (err) return callback(err);
-    
+
     try {
       var original = JSON.parse(data.Body);
     } catch (err) {
       return callback('Error parsing original GeoJSON');
     }
 
-    if (turf[task]) {
-      var final = turf[task](original, 500, 'meters');
+    if (original.type === 'Feature') {
+      var operation = original.properties['turf-lambda:operation'];
+      if (!operation) return callback(new Error('Feature has no turf-lambda:operation'));
+      var parameters = original.properties['turf-lambda:parameters'];
+      if (!parameters) return callback(new Error('Feature has no turf-lambda:parameters'));
+
+      console.log('do we get here?');
+      runTask(original, operation, parameters, function(err, final) {
+        if (err) return callback(err);
+        return callback(null, final);
+      });
+    } else if (original.type === 'FeatureCollection' && original['turf-lambda']) {
+      var operation = original['turf-lambda']['turf-lambda:operation'];
+      var parameters = original['turf-lambda']['turf-lambda:parameters'];
+      runTask(original, operation, parameters, function(err, final) {
+        if (err) return callback(err);
+        return callback(null, final);
+      });
+    } else if (original.type === 'FeatureCollection') {
+      // loop through each individual feature
+      var final = {type: 'FeatureCollection', features: []};
+      original.features.forEach(function(feature) {
+        var featureOperation = feature.properties['turf-lambda:operation'];
+        var featureParameters = feature.properties['turf-lambda:parameters'];
+        if (featureOperations && featureParameters) {
+          runTask(feature, featureOperation, featureParameters, function(err, finalFeature) {
+            if (err) return callback(err);
+            final.features.push(finalFeature);
+          });
+        }
+      });
+
+      if (!final.features.length) return callback(new Error('FeatureCollection features did not have any turf-lambda operations specified.'));
       return callback(null, final);
     } else {
-      return callback(`Turf method (${task}) does not exist.`);
+      return callback(null, 'Not a valid GeoJSON');
     }
+
+
   });
 };
+
+/**
+ * Run a turf task based on an operation
+ * @param {Object} geojson - the geojson object to perform a turf operation
+ * @param {String} operation - a turf operation to run
+ * @param {Array} parameters - the parameters to pass into the turf operation.
+ * The first parameter must be the GeoJSON object
+ * @param {Function} callback
+ */
+function runTask(geojson, operation, parameters, callback) {
+  var applied = buildParameters(geojson, parameters);
+  if (turf[operation]) {
+    try {
+      var final = turf[operation].apply(this, applied);
+      return callback(null, final);
+    } catch (err) {
+      return callback(err);
+    }
+  } else {
+    return callback(`Turf method (${operation}) does not exist.`);
+  }
+}
+
+function buildParameters(gj, parameters) {
+  var finalParams = [gj];
+  parameters.forEach(function(p) { finalParams.push(p); });
+  return finalParams;
+}
--- test/index.test.js
+++ test/index.test.js
@@ -2,13 +2,7 @@ var test = require('tape');
 var AWS = require('aws-sdk-mock');
 
 var handler = require('..').handler;
-var sampleGeojsonPoint = require('./fixtures/geojson-point-feature.json');
-
-// mocks the AWS s3.getObject method, returns a stringified geojson object
-AWS.mock('S3', 'getObject', function(params, callback) {
-  console.log('params: ', params);
-  callback(null, { Body: JSON.stringify(sampleGeojsonPoint) });
-});
+var event = require('./fixtures/event-valid.json');
 
 test('handler: fails with invalid event object', function(t) {
   handler('not an object', {}, function(err, response) {
@@ -19,7 +13,6 @@ test('handler: fails with invalid event object', function(t) {
 });
 
 test('handler: throws without a callback', function(t) {
-  var event = require('./fixtures/event-valid-buffer.json');
   try {
     handler(event, {});
     t.fail('do not get here');
@@ -30,21 +23,55 @@ test('handler: throws without a callback', function(t) {
   }
 });
 
-test('handler: fails with s3 key that has no turf method', function(t) {
-  var event = require('./fixtures/event-invalid-notask.json');
+// test('handler: fails with s3 key that has no turf method', function(t) {
+//   var event = require('./fixtures/event-invalid-notask.json');
+//   handler(event, {}, function(err, response) {
+//     t.ok(err, 'errored');
+//     t.ok(/Turf method \(invalid\) does not exist/.test(err), 'expected message');
+//     t.end();
+//   });
+// });
+
+AWS.mock('S3', 'getObject', function(params, callback) {
+  var gj = require('./fixtures/geojson-feature-no-operation.json');
+  callback(null, { Body: JSON.stringify(gj) });
+});
+
+test('handler: geojson Feature without turf-lambda:operation property fails', function(t) {
   handler(event, {}, function(err, response) {
     t.ok(err, 'errored');
-    t.ok(/Turf method \(invalid\) does not exist/.test(err), 'expected message');
+    console.log(err);
+    t.ok(/Feature has no turf-lambda:operation/.test(err), 'expected error message');
     t.end();
   });
 });
 
-test('handler: successful point buffered', function(t) {
-  var event = require('./fixtures/event-valid-buffer.json');
+test('handler: geojson Feature without turf-lambda:parameters property fails', function(t) {
+  t.end();
+});
+
+test('handler: geojson FeatureCollection without turf-lambda object or Features without turf-lambda properties fails', function(t) {
+  t.end();
+});
+
+AWS.mock('S3', 'getObject', function(params, callback) {
+  var gj = require('./fixtures/geojson-feature-point.json');
+  callback(null, { Body: JSON.stringify(gj) });
+});
+
+test('handler: Feature buffered successfully', function(t) {
   handler(event, {}, function(err, response) {
     t.notOk(err, 'no error');
     t.equal(response.type, 'Feature', 'geojson feature returned');
     t.equal(response.geometry.type, 'Polygon', 'feature is a polygon');
     t.end();
   });
+});
+
+test('handler: FeatureCollection with turf-lambda object buffered successfully', function(t) {
+  t.end();
+});
+
+test('handler: FeatureCollection with each Features properties buffered successfully', function(t) {
+  t.end();
 });
